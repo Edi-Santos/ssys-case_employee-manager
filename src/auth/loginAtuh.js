a1f5c2c9eae4d90b5f64134a99ddbaeb766ae342
Ed Santos <edsantos2822@gmail.com>
2022-03-31 17:11:32 -0300

    feat: criando middleware de autenticacao

--- src/auth/loginAtuh.js
+++ src/auth/loginAtuh.js
@@ -1,7 +1,28 @@
-// const jwt = require('jsonwebtoken');
+const jwt = require('jsonwebtoken');
+const Login = require('../models/Login');
 
 const secret = '!#securityKey#!';
 
+const authorization = async (req, res, next) => {
+  try {
+    const token = req.headers.authorization;
+
+    if (!token) return res.status(401).json({ message: 'Falta token de autorização' });
+    
+    const decoded = jwt.verify(token, secret);
+
+    const userAuth = await Login.findUserByEmail(decoded.data.email);
+
+    if (!userAuth) return res.status(401).json({ message: 'Token malformado' });
+
+    req.user = userAuth;
+    next();
+  } catch (error) {
+   return res.status(401).json({ message: error.message });
+  }
+};
+
 module.exports = {
   secret,
+  authorization,
 };
