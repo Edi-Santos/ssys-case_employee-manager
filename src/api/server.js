6eff63e5a7bc4ade71b17730bed0f0c33fe6a2f6
Ed Santos <edsantos2822@gmail.com>
2022-04-06 13:51:04 -0300

    refacto: refatorando para caso nao existam variaveis de ambiente

--- src/api/server.js
+++ src/api/server.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 const app = require('./app');
 
-const { PORT } = process.env;
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
   console.log(`Conectado na porta ${PORT}`);
